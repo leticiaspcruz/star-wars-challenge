6f7f3787e6bd4e3a9f600455d7bb369536fea554
leticiaspcruz <leticiaspcruz@gmail.com>
2025-03-19 12:46:24 -0300

    removendo consoles

--- my-app/src/components/switchTheme/index.tsx
+++ my-app/src/components/switchTheme/index.tsx
@@ -6,12 +6,7 @@ import { ThemeContext } from '@/providers/ThemeContext';
 const SwitchTheme = () => {
     const themeContext = useContext(ThemeContext);
     const { toggleTheme, theme } = themeContext;
-
-    console.log('themeContext --', themeContext);
-    console.log('theme --', theme);
-    console.log('toggleTheme --', toggleTheme);
-
-    console.log("switch renderizado");
+    
     return (
         <button onClick={toggleTheme}>            
             Mudar tema - Tema atual: {theme}
