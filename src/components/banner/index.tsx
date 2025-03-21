59b7e053fea7673c0c61858b4f64387a33817e6a
leticiaspcruz <leticiaspcruz@gmail.com>
2025-03-21 14:46:31 -0300

    fix: ajustes de erros de console

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -9,21 +9,21 @@ interface BannerProps {
     altText: string;
     text: string;
   }
-  
+
   const Banner: React.FC<BannerProps> = ({ imageUrl, altText, text }) => {
     return (
       <S.BannerContainer>
         <Image
         src={imageUrl}
         alt={altText}
-        layout="fill"  
-        objectFit="cover"   
+        layout="fill"
+        objectFit="cover"
         objectPosition="center"
-        loading="lazy"
+        priority
         />
         <S.BannerText>{text}</S.BannerText>
       </S.BannerContainer>
     );
   };
-  
+
   export default Banner;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from '@/providers/ThemeContext';
 import { NavBar, Footer } from '@/components';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import isPropValid from '@emotion/is-prop-valid';
+import { StyleSheetManager } from 'styled-components';
 
 const instrumentSans = Instrument_Sans({
   subsets: ['latin'],
@@ -15,12 +17,14 @@ const instrumentSans = Instrument_Sans({
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <StyleSheetManager shouldForwardProp={isPropValid}>
       <div className={`${instrumentSans.variable}`}>
         <GlobalStyles />
         <NavBar />
         <Component {...pageProps} />
         <Footer />
       </div>
+      </StyleSheetManager>
     </ThemeProvider>
   );
 }
